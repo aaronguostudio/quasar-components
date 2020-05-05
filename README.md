27af7f905ce06cd4e8756b5bd398a1382f2c95f5
aaronguostudio <aaronguostudio@gmail.com>
2020-05-04 21:46:37 -0600

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Quasar Components (quasar-components)
+
+This project contains components that extends quasar components. I mainly use this library in my work fo boost up my development speed.
+
+## Install the dependencies
+```bash
+yarn
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+yarn run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/components/Avatar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex items-center">
+    <q-avatar :color="color" :size="size">
+      <img v-if="showAvatar" src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+      <div v-else>{{initials}}</div>
+    </q-avatar>
+    <div v-if="showName" class="q-ml-sm">{{name}}</div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    color: {
+      type: String,
+      default: 'grey'
+    },
+    size: {
+      type: String,
+      default: '1.6rem'
+    },
+    firstName: {
+      type: String,
+      default: ''
+    },
+    lastName: {
+      type: String,
+      default: ''
+    },
+    showAvatar: {
+      type: Boolean,
+      default: false
+    },
+    showName: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    name () {
+      return `${this.firstName} ${this.lastName}`
+    },
+    initials () {
+      return `${this.firstName.charAt(0)}${this.lastName.charAt(0)}`
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
